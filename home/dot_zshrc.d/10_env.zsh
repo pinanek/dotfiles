6b178d47c47ca50dd12d4044e729ba9036423a5e
Ngo Duc Hoang Son <hoangsonngo555@gmail.com>
2025-10-28 10:35:15 +0700

    chore: updates

--- home/dot_zshrc.d/10_env.zsh
+++ home/dot_zshrc.d/10_env.zsh
@@ -1,6 +1,8 @@
 # Brew
 if [[ "$(uname)" == "Darwin" ]]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
+elif [[ "$(uname)" == "Linux" ]]; then
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
 # XDG
