aec99495f0f1260cbc155f45e99fb27fe9049a9d
Ngo Duc Hoang Son <hoangsonngo555@gmail.com>
2025-11-02 23:59:14 +0700

    chore: updates

--- scripts/common.sh
+++ scripts/common.sh
@@ -57,7 +57,7 @@ function setup_shpool() {
   log info 'Setting up shpool...'
 
   curl -fLo "${XDG_CONFIG_HOME:-$HOME/.config}/systemd/user/shpool.service" --create-dirs https://raw.githubusercontent.com/shell-pool/shpool/master/systemd/shpool.service
-  sed -i "s|/usr|$HOME/.cargo|" "${XDG_CONFIG_HOME:-$HOME/.config}/systemd/user/shpool.service"
+  sed -i "s|/usr|$XDG_DATA_HOME/mise/installs/cargo-shpool/latest|" "${XDG_CONFIG_HOME:-$HOME/.config}/systemd/user/shpool.service"
   curl -fLo "${XDG_CONFIG_HOME:-$HOME/.config}/systemd/user/shpool.socket" --create-dirs https://raw.githubusercontent.com/shell-pool/shpool/master/systemd/shpool.socket
   systemctl --user enable shpool
   systemctl --user start shpool
