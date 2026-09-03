790dbed7956f5c4ea1541d86d9892b478fce14b9
Ngo Duc Hoang Son <hoangsonngo555@gmail.com>
2026-09-03 22:12:29 +0900

    update

--- home/.chezmoitemplates/tools.bin.sh
+++ home/.chezmoitemplates/tools.bin.sh
@@ -1,19 +1,27 @@
 install_bin_tools() {
+    local source
+    local executable
+    local asset_pattern
+
     if [[ ! -x "$bin_manager" ]]; then
-        log_warning 'Cannot install development binary tools: bin is unavailable.'
+        log_warning 'Cannot install binary tools: bin is unavailable.'
         return
     fi
 
     while read -r source executable asset_pattern; do
-        [[ -n "$source" && -n "$executable" && "$source" != \#* ]] || continue
+        [[ -n "$source" && "$source" != \#* && -n "$executable" ]] || continue
 
         if [[ -x "$local_bin/$executable" ]]; then
             log_info "$executable is already installed."
-        else
+        elif [[ -n "$asset_pattern" ]]; then
             log_info "Installing $executable with bin..."
             env PATH="$local_bin:$PATH" \
                 "$bin_manager" install --name "$asset_pattern" \
                 "$source" "$local_bin/$executable"
+        else
+            log_info "Installing $executable with bin..."
+            env PATH="$local_bin:$PATH" \
+                "$bin_manager" install "$source" "$local_bin/$executable"
         fi
     done
 }
